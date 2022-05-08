a7960e3ccdfb0d22af41aac64f9cc0793fe29da1
psyk2642 <60137590+psyk2642@users.noreply.github.com>
2022-05-08 12:53:07 +0800

    Update AssemblyInfo.vb

--- iBMSC/My Project/AssemblyInfo.vb
+++ iBMSC/My Project/AssemblyInfo.vb
@@ -30,7 +30,7 @@ Imports System.Resources
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.5.5.4")>
-<Assembly: AssemblyFileVersion("3.5.5.4")>
+<Assembly: AssemblyVersion("3.5.5.5")>
+<Assembly: AssemblyFileVersion("3.5.5.5")>
 
 <Assembly: NeutralResourcesLanguageAttribute("en")>
